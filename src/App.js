4b4c1e2622b1e6aa63cbeafb9afee21f47e0cd3a
b0urbaki7 <luisgarcian595@gmail.com>
2023-07-16 20:28:17 -0500

    Boceto página principal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { Icon, InputAdornment, TextField, Typography } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+
+function Buscador(){
+  return(
+    <div>
+      <TextField 
+        label="Ingrese su búsqueda"
+        InputProps={{endAdornment:(<InputAdornment position='start'><SearchIcon></SearchIcon></InputAdornment>),}}
+        variant="standard"
+        sx={{ input: { color: 'white'},'& label.Mui-focused': {
+          color: '#A0AAB4',
+        },
+        '& .MuiInput-underline:after': {
+          borderBottomColor: '#B2BAC2',
+        },
+        '& .MuiOutlinedInput-root': {
+          '& fieldset': {
+            borderColor: '#E0E3E7',
+          },
+          '&:hover fieldset': {
+            borderColor: '#B2BAC2',
+          },
+          '&.Mui-focused fieldset': {
+            borderColor: '#6F7E8C',
+          },
+        }, }}>
+      </TextField>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant='h1'>PumaTesis</Typography>
+        <Typography variant='subtitle1'>El buscador de tesis más avanzado del mundo</Typography>
+        <Buscador/>
       </header>
+      
     </div>
   );
 }
